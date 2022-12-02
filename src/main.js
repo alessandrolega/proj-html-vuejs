bbe896f4a8e6c2beb547f31acfa9f6db9fed12f7
alessandrolega <alessandrolega24@gmail.com>
2022-12-02 19:35:01 +0100

    init vue + push img + import font awesome

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+  /* import the fontawesome core */
+  import { library } from '@fortawesome/fontawesome-svg-core'
+
+  /* import font awesome icon component */
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+  import {
+    faCartShopping,
+    faMagnifyingGlass,
+  } from "@fortawesome/free-solid-svg-icons";
+
+  // Import icone regular /
+import {} from "@fortawesome/free-regular-svg-icons";
+
+// add icons to the library /
+library.add(faCartShopping, faMagnifyingGlass);
+// add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
