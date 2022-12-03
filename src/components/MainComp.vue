6d3bc8866471d931c15bfa8578bc07390e1740ed
alessandrolega <alessandrolega24@gmail.com>
2022-12-03 02:05:39 +0100

    creati components macroaree

---
+++ src/components/MainComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Ciao</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MainComp'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
