baa8b1652cfb460fea80277b29512c4e7be19522
alessandrolega <alessandrolega24@gmail.com>
2022-12-06 20:09:38 +0100

    completato footer + parte 2 interna

---
+++ src/components/footer/FooterElement.vue
@@ -0,0 +1,134 @@
+<template>
+    
+    <!--INIZIO FOOTER-->
+    <footer class="footer-section">
+        <!--CONTAINER-->
+        <div class="container">
+            <!--FOOTER CONT-->
+            <div class="footer-cont">
+                <!--FOOTER CARDS-->
+                <div class="card">
+                    <!--CARD HEAD-->
+                    <div class="card-head">
+                        <img src="../../assets/img/logo.png" alt="logo">
+                    </div>
+                    <!--CARD BODY-->
+                    <div class="card-body">
+                        <p>Ac aliquam neque sagittis diam faucibus vitae
+                            purus turpis phasellus. Pellentesque consectetur amet
+                            purus ultrices mauris.
+                        </p>
+                    </div>
+                    <!--CARD FOOTER-->
+                    <div class="card-footer">
+                        <!--ICON CONT-->
+                        <font-awesome-icon icon="fa-brands fa-facebook-f" class="icon-footer" />
+                        <font-awesome-icon icon="fa-brands fa-twitter" class="icon-footer" />
+                        <font-awesome-icon icon="fa-brands fa-youtube" class="icon-footer" />
+                        <font-awesome-icon icon="fa-brands fa-instagram" class="icon-footer" />
+                        <font-awesome-icon icon="fa-brands fa-linkedin-in" class="icon-footer" />
+                    </div>
+                </div>
+                <div class="card">
+                    <h3>RECENT POSTS</h3>
+                    <!--CARD BODY-->
+                <div class="card-body">
+                    <!--FOOTER MENU DINAMICO-->
+                    <ul>
+                        <li v-for="(element, index) in footerNav"
+                        :key="index"
+                        :footernav="element"><span><font-awesome-icon icon="fa-solid fa-angle-right" /></span> {{element.name}}</li>
+                    </ul>
+                </div>
+                </div>
+                <div class="card">
+                    <h3>CONTACT INFORMATIONS</h3>
+                    <p>4746 Tipple Road Michigan 48449</p>
+                    <span>Mobile : 1.800.000.0000</span>
+                    <span>Email : info@your-company.com</span>
+                </div>
+            </div>
+        </div>        
+    </footer>  
+</template>
+
+<script>
+
+    export default {
+        name: 'FooterElement',
+        props: {
+            footerNav: Array
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.container{
+    width: 70%;
+    margin: 0 auto;
+}
+
+.footer-section{
+    background-color: black;
+    padding-bottom: 100px;
+}
+
+.footer-cont{
+    display: flex;
+    gap: 120px;
+    padding-top: 100px;
+}
+
+.card{
+    width: calc(100% / 3);
+    text-align: center;
+}
+
+.card h3{
+    color: white;
+}
+.card-body p{
+    text-align: center;
+    padding: 20px;
+    font-size: 1.1rem;
+    color: #979798;
+    font-weight: bold;
+}
+
+.card p{
+    text-align: center;
+    padding: 20px;
+    font-size: 1.1rem;
+    color: #979798;
+    font-weight: bold;
+}
+
+.card span{
+    text-align: center;
+    padding: 5px;
+    font-size: 1.1rem;
+    color: white;
+    font-weight: bold;
+    display: inline-block;
+}
+
+.card-footer{
+    margin-top: 30px;
+}
+
+.icon-footer{
+    color: #FF414B;
+    padding-left: 5px;
+    padding-right: 5px;
+}
+
+ul  li {
+    color: white;
+    padding-top: 20px;
+}
+
+
+
+
+</style>
---
+++ src/components/footer/FooterSecond.vue
@@ -0,0 +1,58 @@
+<template>
+    <!--INIZIO SEZIONE-->
+    <div class="section-second-footer">
+        <!--TEXT CONT-->
+        <div class="text-container-center">
+            <!--SPAN-->
+            <span class="span-grey">© Copyright 2012 - 2020 | Avada theme by <span class="span-white">ThemeFusion</span>
+            | All rights reserved | Powered by <span class="span-white">Alessandro Lega</span></span>
+        </div>
+        <!--ARROW UP-->
+        <button class="btn-arrow"><font-awesome-icon icon="fa-solid fa-angle-up" /></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterSecond'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.section-second-footer{
+    background: black;
+    border-top: 1px solid grey;
+    padding: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+}
+
+.text-container-center{
+    text-align: center;
+    width: 40%;
+}
+
+.span-white{
+    color: white;
+}
+
+.span-grey{
+    color: grey;
+}
+
+.btn-arrow{
+    width: 40px;
+    height: 30px;
+    border:none;
+    border-radius: 5px;
+    background-color: #2F2F2F;
+    color: white;
+    position: absolute;
+    bottom: 0;
+    right: 70px;
+}
+
+</style>
